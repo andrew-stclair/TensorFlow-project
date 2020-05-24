8bbe091efc015bb393ee71776afa347fa2c73895
Andrew St Clair <andrew_stclair95@hotmail.com>
2020-05-24 14:46:03 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
-# This file will take a webcam input and put it through object detection
-# and output a feed with boxes indicating the detected object
-# To Do
-# TODO: Add requirements.txt
-# TODO: Write code
+# import tensorflow as tf
+import cv2
+
+video = cv2.VideoCapture(1)
+
+cv2.namedWindow("input")
+
+img_counter = 0
+
+while True:
+    ret, frame = video.read()
+    cv2.imshow("input", frame)
+    if not ret:
+        break
+    k = cv2.waitKey(1)
+
+    if k % 256 == 27:
+        # ESC Pressed
+        break
+    elif k % 256 == 32:
+        # SPACE pressed
+        img_name = "opencv_frame_{}.png".format(img_counter)
+        cv2.imwrite(img_name, frame)
+        img_counter += 1
+
+video.release()
+
+cv2.destroyAllWindows()
